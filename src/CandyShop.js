c8434e98020ec9664467e6824aa1af03916b874d
Ada <byjean94@gmail.com>
2022-03-28 11:48:53 +0200

    Added Logo

--- src/CandyShop.js
+++ src/CandyShop.js
@@ -1,12 +1,20 @@
-import './App.css';
+import styles from './App.module.css';
 import { NavigationBar } from './NavBar';
+import Logo from './logowhole.png'
 
 function App() {
   return (
     <div>
       <NavigationBar />
-      <div className="App">
-        Hello!
+      <div className={styles.FrontPage}>
+        <img className={styles.logoBig} src={Logo} alt="Duże logo sklepu ze słodyczami" />
+        <div className={styles.information}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar vitae eros id consectetur.
+          Curabitur cursus sed sem in porttitor. In arcu massa, fermentum a mauris eu, pulvinar faucibus nulla.
+          Donec nec massa eget elit efficitur auctor. Duis sed convallis magna. In vel mollis purus.
+          Suspendisse dapibus ullamcorper eros, sit amet facilisis justo lacinia eu. Cras sit amet ante ante.
+          Donec mollis tristique urna quis hendrerit. Ut et euismod est. Mauris venenatis risus non eros volutpat gravida.
+          Donec dui libero, cursus ac justo eu, suscipit fermentum orci. Nulla sed consequat eros.
+          Donec metus neque, pellentesque bibendum augue eget, efficitur laoreet turpis.</div>
       </div>
     </div>
   );
--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -1,19 +1,15 @@
 import styles from './NavBar.module.css';
-import { Navbar } from 'react-bootstrap';
-import { Container } from 'react-bootstrap';
-import { Nav } from 'react-bootstrap';
+import Logo from '../logosmall.png'
 
 export function NavigationBar() {
     return <div className={styles.navigation}>
-        <Navbar bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="#home" className={styles.options}>Sklep</Nav.Link>
-                    <Nav.Link href="#features" className={styles.options}>O nas</Nav.Link>
-                    <Nav.Link href="#pricing" className={styles.options}>Kontakt</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
+        <div>
+            <img className={styles.logoSmall} src={Logo} alt="Logo" />
+        </div>
+        <div className={styles.optionsDiv}>
+            <div className={styles.options}>Sklep</div>
+            <div className={styles.options}>O nas</div>
+            <div className={styles.options}>Kontakt</div>
+        </div>
     </div>
 }
