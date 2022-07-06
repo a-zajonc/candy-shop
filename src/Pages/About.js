00c8deb12de06cd8e4ee4a08d68551752a4f1d33
Ada <byjean94@gmail.com>
2022-07-06 12:29:42 +0200

    Updated About Page to Bootstrap

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,20 +1,20 @@
-import styles from './pages.module.css';
 
 export function About() {
-    return <div className={styles.about}>
-        <img className={styles.aboutPicture}
+    return <div className="d-flex flex-column p-2 align-items-center">
+        <img className="rounded"
             src="https://png.pngitem.com/pimgs/s/56-560573_working-woman-illustration-png-transparent-png.png"
-            alt="Obrazek pracowników" />
-        <br></br>
-        <span>Jesteśmy firmą lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vestibulum tortor justo, varius at pharetra et,
-            sollicitudin at neque. Maecenas a lorem ipsum.
-            Nulla facilisi. Nullam tempor nulla ut porttitor tristique.
-            Donec eget nisl pellentesque, placerat urna vel, venenatis odio. Aliquam et purus massa.
-            Aenean suscipit dui a lobortis hendrerit. Aliquam consectetur mauris et nunc pharetra, in suscipit elit efficitur.
-            In pharetra ultricies enim sit amet pretium. Cras in semper nunc. Ut sed venenatis nunc, nec interdum nisi.
-            Aenean ante enim, rutrum id ullamcorper sed, interdum eu massa. Quisque volutpat libero at augue lacinia sodales.
-            Donec ut rhoncus sem</span>
+            alt="Obrazek pracownika" />
+        <div className="card m-2">
+            <div className="card-body fs-5" >Jesteśmy firmą lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum tortor justo, varius at pharetra et,
+                sollicitudin at neque. Maecenas a lorem ipsum.
+                Nulla facilisi. Nullam tempor nulla ut porttitor tristique.
+                Donec eget nisl pellentesque, placerat urna vel, venenatis odio. Aliquam et purus massa.
+                Aenean suscipit dui a lobortis hendrerit. Aliquam consectetur mauris et nunc pharetra, in suscipit elit efficitur.
+                In pharetra ultricies enim sit amet pretium. Cras in semper nunc. Ut sed venenatis nunc, nec interdum nisi.
+                Aenean ante enim, rutrum id ullamcorper sed, interdum eu massa. Quisque volutpat libero at augue lacinia sodales.
+                Donec ut rhoncus sem</div>
+        </div>
     </div>
 
 }
